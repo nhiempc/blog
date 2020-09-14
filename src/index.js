2ff488f9294051306a3dd47b93c76a5528ec0b4d
nhiempc <nhiem10596@gmail.com>
2020-09-14 13:25:43 +0700

    setup MVC core project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+app.engine('hbs',handlebars({extname: 'hbs'}));
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,'resources/views'));
+
+route(app);
+
+app.listen(port, function(error){
+    if (error) {
+        console.log("Something went wrong");
+    }
+    console.log("server is running port:  " + port);
+});
